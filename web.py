dd2c1e76d76b0ef96327154f1057aea4b6ecdff9
James Patrick Keegan <atomicbomber.email@gmail.com>
2016-06-16 11:01:14 +0700

    Main page now can display map of the proper location.

--- web.py
+++ web.py
@@ -5,18 +5,10 @@ app = Bottle()
 
 @app.route('/')
 def get_content():
-	return static_file('main_page.html', './')
+	return static_file('main.html', './')
 
 @app.route('/<static_path:path>')
 def get_static_resource(static_path):
 	return static_file(static_path, './')
-
-import sqlite3
-
-# Temporary ajax response handlers
-@app.route('/ajax')
-def get_ajax_response():
-	# If return type is dictionary, it will be automatically converted into JSON
-	return { "name": "James", "address": "Nowhere"}
 
 app.run(reload=True)
