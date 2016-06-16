2336de6f81d3861ac73a45a583b935ec627ae39f
James Patrick Keegan <atomicbomber.email@gmail.com>
2016-06-16 23:46:14 +0700

    main.html will now be rendered using jinja2 before it is sent. Added waypoint table which contains HTML table rows used to reload table view in main.html every time the data is updated, i.e. a new waypoint is inserted. web.py modified to allow jinja2 rendering of template files.

--- web.py
+++ web.py
@@ -1,11 +1,27 @@
 import sqlite3
+
+from jinja2 import Template, Environment, PackageLoader
 from bottle import Bottle, request, static_file
+
+# Load jinja environment
+env = Environment(loader=PackageLoader(__name__, '.'))
+
 
 app = Bottle()
 
 @app.route('/')
 def get_content():
-	return static_file('main.html', './')
+
+	# Create database connection
+	connection = sqlite3.connect("data.sqlite")
+	cursor = connection.cursor()
+	cursor.execute("SELECT name, latitude, longitude, description FROM waypoints")
+	data = cursor.fetchall()
+
+	# Load and render template
+	template = env.get_template("main.html")
+
+	return template.render(records=data)
 
 @app.route('/<static_path:path>')
 def get_static_resource(static_path):
@@ -13,6 +29,35 @@ def get_static_resource(static_path):
 
 @app.route('/add_waypoint', method="POST")
 def add_waypoint():
-	return request.POST.get("lat", "CANT GET")
+	name = request.POST.get("name", "name_error")
+	lat = request.POST.get("lat", "latitude_error")
+	lng = request.POST.get("lng", "longitude_error")
+	description = request.POST.get("desc", "desc_error")
+
+	# Store waypoint to database
+	store_waypoint(name, lat, lng, description)
+
+	### Load refreshed table
+	# Create database connection
+	connection = sqlite3.connect("data.sqlite")
+	cursor = connection.cursor()
+	cursor.execute("SELECT name, latitude, longitude, description FROM waypoints")
+	data = cursor.fetchall()
+
+	# Load and render template
+	template = env.get_template("waypoint_table.html")
+
+	return template.render(records=data)
+	
+
+
+def store_waypoint(name, lat, lng, description):
+	connection = sqlite3.connect("data.sqlite")
+	cursor = connection.cursor()
+
+	query = "INSERT INTO waypoints(name, latitude, longitude, description) VALUES('{}', {}, {}, '{}')".format(name, lat, lng, description)
+
+	cursor.execute(query)
+	connection.commit()
 
 app.run(reload=True)
